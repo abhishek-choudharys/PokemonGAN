2e1870944edb155f3d98d004ac3a269ad516ffaa
Abhishek Choudhary <35888742+abhishek-choudharys@users.noreply.github.com>
2020-05-09 02:37:24 +0530

    Create resize.py
    File create to resize images to 256*256

---
+++ resize.py
@@ -0,0 +1,12 @@
+import os
+import cv2
+
+src = "Pokemon_GAN/data"
+dest = "resizedData"
+
+os.mkdir(dest)
+
+for i in os.listdir(src):
+    img = cv2.imread(os.path.join(src, i))
+    img = cv2.resize(img, (256, 256))
+    cv2.imwrite(os.path.join(dest,i), img)
