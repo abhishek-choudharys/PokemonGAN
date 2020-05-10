6d531f509f4806d3ab31e593c64a775ed556817a
Abhishek Choudhary <35888742+abhishek-choudharys@users.noreply.github.com>
2020-05-10 13:54:25 +0530

    Update resize.py

--- resize.py
+++ resize.py
@@ -9,4 +9,4 @@ os.mkdir(dest)
 for i in os.listdir(src):
     img = cv2.imread(os.path.join(src, i))
     img = cv2.resize(img, (256, 256))
-    cv2.imwrite(os.path.join(dest,i), img)
+    cv2.imwrite(os.path.join(dest,i), img) #resized images are written in a new folder
