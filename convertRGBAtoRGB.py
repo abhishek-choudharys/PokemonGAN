27aedaa82f65beb560098911850d75926d9b8624
Abhishek Choudhary <35888742+abhishek-choudharys@users.noreply.github.com>
2020-05-15 21:16:39 +0530

    Update convertRGBAtoRGB.py

--- convertRGBAtoRGB.py
+++ convertRGBAtoRGB.py
@@ -1,12 +1,28 @@
+import argparse
+parser = argparse.ArgumentParser(description='Choose dest')
+
+parser.add_argument('--type', dest='type', type=str, default='WGAN', help='choose destination folder')
+
+args = parser.parse_args()
+params = vars(args)
+
+import os
+import cv2
+
+dest = "WGAN/resized_RGB"
+
+if params['type'] == 'WGAN':
+    dest = "WGAN/resized_RGB"
+elif params['type'] == 'GAN':
+    dest = "GAN/resized_RGB"
+   
+
 import os
 from PIL import Image
 
 src = "resizedData"
-dest = "resized_RGB"
-dest2 = "WGAN/resized_RGB"
 
 os.mkdir(dest)
-os.mkdir(dest2)
 
 for i in os.listdir(src):
     img = Image.open(os.path.join(src, i))
@@ -16,8 +32,8 @@ for i in os.listdir(src):
         bg = Image.new("RGB", img.size, (0,0,0))
         bg.paste(img, mask = img.split()[3]) #we paste img in bg using alpha channel as mask
         bg.save(os.path.join(dest, i.split('.')[0] + '.jpg'), 'JPEG') #save the images as jpeg, split dest at '.' and take first split
-        bg.save(os.path.join(dest2, i.split('.')[0] + '.jpg'), 'JPEG') #save the images as jpeg, split dest at '.' and take first split
+        #bg.save(os.path.join(dest2, i.split('.')[0] + '.jpg'), 'JPEG') #save the images as jpeg, split dest at '.' and take first split
     else:
         img.convert('RGB')
         img.save(os.path.join(dest,i.split('.')[0] + '.jpg'), 'JPEG') #save the images as jpeg
-        img.save(os.path.join(dest2,i.split('.')[0] + '.jpg'), 'JPEG') #save the images as jpeg
+        #img.save(os.path.join(dest2,i.split('.')[0] + '.jpg'), 'JPEG') #save the images as jpeg
