58a99d62718e08246079641084d01cc7fadbdde0
Abhishek Choudhary <35888742+abhishek-choudharys@users.noreply.github.com>
2020-05-15 18:46:20 +0530

    Update convertRGBAtoRGB.py

--- convertRGBAtoRGB.py
+++ convertRGBAtoRGB.py
@@ -3,6 +3,7 @@ from PIL import Image
 
 src = "resizedData"
 dest = "resized_RGB"
+dest2 = "WGAN/resized_RGB"
 
 os.mkdir(dest)
 
@@ -14,6 +15,8 @@ for i in os.listdir(src):
         bg = Image.new("RGB", img.size, (0,0,0))
         bg.paste(img, mask = img.split()[3]) #we paste img in bg using alpha channel as mask
         bg.save(os.path.join(dest, i.split('.')[0] + '.jpg'), 'JPEG') #save the images as jpeg, split dest at '.' and take first split
+        bg.save(os.path.join(dest2, i.split('.')[0] + '.jpg'), 'JPEG') #save the images as jpeg, split dest at '.' and take first split
     else:
         img.convert('RGB')
         img.save(os.path.join(dest,i.split('.')[0] + '.jpg'), 'JPEG') #save the images as jpeg
+        img.save(os.path.join(dest2,i.split('.')[0] + '.jpg'), 'JPEG') #save the images as jpeg
